944de58714af9206040c1d87f057f3df84056796
Gleb Dovzhenko <dovjenko.g@gmail.com>
2023-12-05 12:27:12 +0700

    Added nvim-tree tab offset

--- nvim/lua/core/plugin_config/bufferline.lua
+++ nvim/lua/core/plugin_config/bufferline.lua
@@ -1,5 +1,13 @@
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "NvimTree",
+                highlight = "Directory",
+                separator = true
+            }
+        }
+    }
+}
 
---vim.keymap.set('n', '<leader>1', ':bp<CR>')
---vim.keymap.set('n', '<leader>2', ':bn<CR>')
---vim.keymap.set('n', '<leader>w', ':bd<CR>')
