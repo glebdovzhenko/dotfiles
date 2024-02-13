e676ddb69cc8dd6b503a81906be522d8f7fec05d
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-02-13 11:41:03 +0700

    moved to lazy.nvim

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,58 +1,91 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-
-  use 'wbthomason/packer.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'lervag/vimtex'
-  use 'preservim/nerdcommenter'
-  use 'morhetz/gruvbox'
-  use 'gelguy/wilder.nvim'
-  use 'romgrk/fzy-lua-native'
-  use 'lewis6991/gitsigns.nvim'
-  use 'mrjones2014/legendary.nvim'
-  use 'stevearc/dressing.nvim'
-  use 'karb94/neoscroll.nvim'
+  --use 'wbthomason/packer.nvim'
+  --use 'nvim-tree/nvim-tree.lua'
+  --use 'lervag/vimtex'
+  --use 'preservim/nerdcommenter'
+  --use 'morhetz/gruvbox'
+  --use 'gelguy/wilder.nvim'
+  --use 'romgrk/fzy-lua-native'
+  --use 'lewis6991/gitsigns.nvim'
+  --use 'mrjones2014/legendary.nvim'
+  --use 'stevearc/dressing.nvim'
+  --use 'karb94/neoscroll.nvim'
   
-  use {
-      'neoclide/coc.nvim', 
+  --use {
+  --    'neoclide/coc.nvim', 
+  --    branch = 'release',
+  --    run = ':CocInstall coc-go coc-pyright coc-prettier coc-json coc-lua coc-html coc-html-css-support'
+  --}
+  --use {
+  --    'nvim-lualine/lualine.nvim', 
+  --    requires = {
+  --        'nvim-tree/nvim-web-devicons', 
+  --        {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate lua c vim vimdoc query'}
+  --    }
+  --}
+  --use {
+  --    'akinsho/bufferline.nvim', 
+  --    tag = "v3.*", 
+  --    requires = 'nvim-tree/nvim-web-devicons'
+  --}
+  --use {
+  --    'nvim-telescope/telescope.nvim', 
+  --    tag = '0.1.4', 
+  --    requires = 'nvim-lua/plenary.nvim'
+  --}
+  --use {
+  --    "akinsho/toggleterm.nvim", tag = '*', 
+  --}
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require("lazy").setup({
+  'wbthomason/packer.nvim',
+  'nvim-tree/nvim-tree.lua',
+  'lervag/vimtex',
+  'preservim/nerdcommenter',
+  'morhetz/gruvbox',
+  'gelguy/wilder.nvim',
+  'romgrk/fzy-lua-native',
+  'lewis6991/gitsigns.nvim',
+  'mrjones2014/legendary.nvim',
+  'stevearc/dressing.nvim',
+  'karb94/neoscroll.nvim',
+  {
+      'neoclide/coc.nvim',
       branch = 'release',
-      run = ':CocInstall coc-go coc-pyright coc-prettier coc-json coc-lua coc-html coc-html-css-support'
-  }
-  use {
-      'nvim-lualine/lualine.nvim', 
-      requires = {
+      build = ':CocInstall coc-go coc-pyright coc-prettier coc-json coc-lua coc-html coc-html-css-support'
+  },
+  {
+      'nvim-lualine/lualine.nvim',
+      dependencies = {
           'nvim-tree/nvim-web-devicons', 
-          {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate lua c vim vimdoc query'}
+          {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate lua c vim vimdoc query'}
       }
-  }
-  use {
+  },
+  {
       'akinsho/bufferline.nvim', 
-      tag = "v3.*", 
-      requires = 'nvim-tree/nvim-web-devicons'
+      version = "v3.*", 
+      dependencies = 'nvim-tree/nvim-web-devicons'
+  },
+  {
+      'nvim-telescope/telescope.nvim',
+      version = '0.1.4',
+      dependencies = 'nvim-lua/plenary.nvim'
+  },
+  {
+      "akinsho/toggleterm.nvim", version = '*'
   }
-  use {
-      'nvim-telescope/telescope.nvim', 
-      tag = '0.1.4', 
-      requires = 'nvim-lua/plenary.nvim'
-  }
-  use {
-      "akinsho/toggleterm.nvim", tag = '*', 
-  }
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
-
+})
