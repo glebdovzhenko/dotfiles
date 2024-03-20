9285e3ddceb3676f88a40c4f28891895f9f78512
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-03-20 12:25:13 +0700

    Deleted old comments in plugins.lua

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,40 +1,4 @@
-  --use 'wbthomason/packer.nvim'
-  --use 'nvim-tree/nvim-tree.lua'
-  --use 'lervag/vimtex'
-  --use 'preservim/nerdcommenter'
-  --use 'morhetz/gruvbox'
-  --use 'gelguy/wilder.nvim'
-  --use 'romgrk/fzy-lua-native'
-  --use 'lewis6991/gitsigns.nvim'
-  --use 'mrjones2014/legendary.nvim'
-  --use 'stevearc/dressing.nvim'
-  --use 'karb94/neoscroll.nvim'
-  
-  --use {
-  --    'neoclide/coc.nvim', 
-  --    branch = 'release',
-  --    run = ':CocInstall coc-go coc-pyright coc-prettier coc-json coc-lua coc-html coc-html-css-support'
-  --}
-  --use {
-  --    'nvim-lualine/lualine.nvim', 
-  --    requires = {
-  --        'nvim-tree/nvim-web-devicons', 
-  --        {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate lua c vim vimdoc query'}
-  --    }
-  --}
-  --use {
-  --    'akinsho/bufferline.nvim', 
-  --    tag = "v3.*", 
-  --    requires = 'nvim-tree/nvim-web-devicons'
-  --}
-  --use {
-  --    'nvim-telescope/telescope.nvim', 
-  --    tag = '0.1.4', 
-  --    requires = 'nvim-lua/plenary.nvim'
-  --}
-  --use {
-  --    "akinsho/toggleterm.nvim", tag = '*', 
-  --}
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -49,8 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-
 require("lazy").setup({
   'wbthomason/packer.nvim',
   'nvim-tree/nvim-tree.lua',
@@ -63,6 +25,7 @@ require("lazy").setup({
   'mrjones2014/legendary.nvim',
   'stevearc/dressing.nvim',
   'karb94/neoscroll.nvim',
+  'sheerun/vim-polyglot',
   {
       'neoclide/coc.nvim',
       branch = 'release',
