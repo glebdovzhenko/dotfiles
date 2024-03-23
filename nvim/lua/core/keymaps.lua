bcea2fcdc651e2c40e18065d4e99629b2c602a14
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-03-23 11:26:09 +0700

    removed legendary nvim

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,129 +14,137 @@ vim.api.nvim_command('retab')
 vim.cmd('filetype plugin indent on')
 vim.opt.syntax = 'enable'
 
+-- general -----------------------------------------------------------------
+-- Moving code blocks in visual mode
+vim.keymap.set('v', 'J',
+    ":m '>+1<CR>gv=gv",
+    { desc = 'Move code block one line down' })
+vim.keymap.set('v', 'K',
+    ":m '<-2<CR>gv=gv",
+    { desc = 'Move code block one line up' })
+-- cursor stays in the middle of screen when jumping
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+-- search terms are in the middle of screen when jumping
+vim.keymap.set('n', 'n', "nzzzv")
+vim.keymap.set('n', 'N', "Nzzzv")
+-- paste over selection without overwriting copy buffer
+vim.keymap.set('x', '<leader>p',
+    "\"_dP",
+    { desc = 'Paste without overwriting the buffer' })
+-- copying to system clipboard
+vim.keymap.set('n', '<leader>y',
+    "\"+y",
+    { desc = 'Yank to system clipboard' })
+vim.keymap.set('v', '<leader>y',
+    "\"+y",
+    { desc = 'Yank to system clipboard' })
+vim.keymap.set('n', '<leader>Y',
+    "\"+Y",
+    { desc = 'Yank to system clipboard' })
+-- deleting to void register
+vim.keymap.set('n', '<leader>d',
+    "\"_d",
+    { desc = 'Delete to void register' })
+vim.keymap.set('v', '<leader>d',
+    "\"_d",
+    { desc = 'Delete to void register' })
+-- search and replace the current word
+--vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-require('legendary').setup({
-    keymaps = {
-        -- legendary ----------------------------------------------------------
-        {
-            '<leader>H',
-            ':Legendary<CR>',
-            description = 'Run command search'
-        },
+-- ToggleTerm -----------------------------------------------------------------
 
-        -- ToggleTerm ---------------------------------------------------------
-        {
-            '<leader>g',
-            function()
-                local term = require('toggleterm.terminal').Terminal
-                local lg = term:new({
-                    cmd = "lazygit",
-                    hidden = true,
-                    direction = "float"
-                })
-                lg:toggle()
-            end,
-            description = 'Lazygit over ToggleTerm floating terminal'
-        },
+vim.keymap.set('n', '<leader>g',
+    function()
+        local term = require('toggleterm.terminal').Terminal
+        local lg = term:new({
+            cmd = "lazygit",
+            hidden = true,
+            direction = "float"
+        })
+        lg:toggle()
+    end,
+    { desc = "Lazygit over ToggleTerm floating terminal" })
 
-        -- Telescope ----------------------------------------------------------
-        {
-            '<leader>F',
-            function()
-                local tb = require('telescope.builtin')
-                tb.find_files()
-            end,
-            description = 'Telescope find files'
-        },
-        {
-            '<leader>f',
-            function()
-                local tb = require('telescope.builtin')
-                tb.live_grep()
-            end,
-            description = 'Telescope live grep'
-        },
-        {
-            '<leader>h',
-            function()
-                local tb = require('telescope.builtin')
-                tb.help_tags()
-            end,
-            description = 'Telescope help tags'
-        },
+-- Telescope ------------------------------------------------------------------
+vim.keymap.set('n', '<leader>F',
+    function()
+        local tb = require('telescope.builtin')
+        tb.find_files()
+    end,
+    { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>f',
+    function()
+        local tb = require('telescope.builtin')
+        tb.live_grep()
+    end,
+    { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>h',
+    function()
+        local tb = require('telescope.builtin')
+        tb.help_tags()
+    end,
+    { desc = 'Telescope help tags' })
 
-        -- nvim-tree ----------------------------------------------------------
-        { '<leader>`', ':NvimTreeToggle<CR>',       description = 'Toggle nvim-tree' },
-        { '<leader>R', ':NvimTreeRefresh<CR>',      description = 'Refresh nvim-tree' },
+-- nvim-tree ------------------------------------------------------------------
+vim.keymap.set('n', '<leader>`',
+    ':NvimTreeToggle<CR>',
+    { desc = 'Toggle nvim-tree' })
+vim.keymap.set('n', '<leader>R',
+    ':NvimTreeRefresh<CR>',
+    { desc = 'Refresh nvim-tree' })
 
-        -- bufferline ---------------------------------------------------------
-        { '<leader>1', ':bp<CR>',                   description = 'Prev buffer' },
-        { '<leader>2', ':bn<CR>',                   description = 'Next buffer' },
-        { '<leader>w', ':bd<CR>',                   description = 'Close buffer' },
+-- bufferline -----------------------------------------------------------------
+vim.keymap.set('n', '<leader>1',
+    ':bp<CR>',
+    { desc = 'Prev buffer' })
+vim.keymap.set('n', '<leader>2',
+    ':bn<CR>',
+    { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>w',
+    ':bd<CR>',
+    { desc = 'Close buffer' })
 
-        -- LSP ----------------------------------------------------------------
-        -- TODO: move to only when LSP is attached
-        { 'gD',        vim.lsp.buf.declaration,     description = 'Find declaration' },
-        { 'gd',        vim.lsp.buf.definition,      description = 'Find definition' },
-        { 'K',         vim.lsp.buf.hover,           description = 'Show help' },
+-- NERDCommenter --------------------------------------------------------------
+vim.keymap.set('n', '<leader>cl',
+    '<Plug>NERDCommenterAlignLeft',
+    { silent = true, desc = 'Comment line(s) with delimiters aligned left.' })
+vim.keymap.set('x', '<leader>cl',
+    '<Plug>NERDCommenterAlignLeft',
+    { silent = true, desc = 'Comment line(s) with delimiters aligned left.' })
+vim.keymap.set('n',
+    '<leader>cu',
+    '<Plug>NERDCommenterUncomment',
+    { desc = 'Uncomment line(s).', silent = true })
+vim.keymap.set('x',
+    '<leader>cu',
+    '<Plug>NERDCommenterUncomment',
+    { silent = true, desc = 'Uncomment line(s).' })
 
-        -- diagnostics
-        { '<space>e',  vim.diagnostic.open_float,   description = 'Open diagnostics' },
-        { '<space>q',  vim.diagnostic.set_loc_list, description = 'List diagnostics' },
-        { '[d',        vim.diagnostic.goto_prev,    description = 'GoTo previous diagnoctic' },
-        { ']d',        vim.diagnostic.goto_next,    description = 'GoTo next diagnostic' },
+--vim.keymap.set('n',
+--    '<leader>cy',
+--    '<Plug>NERDCommenterYank',
+--    { silent = true, description = 'Comment line(s) and yank the selection.' })
+--vim.keymap.set('x',
+--    '<leader>cy',
+--    '<Plug>NERDCommenterYank',
+--    { silent = true, description = 'Comment line(s) and yank the selection.' })
 
-        {
-            '<space>f',
-            function()
-                vim.lsp.buf.format { async = true }
-            end,
-            description = 'Format current buffer'
-        },
+---- TODO: move to only when LSP is attached
+--{ 'gD',       vim.lsp.buf.declaration,     description = 'Find declaration' },
+--{ 'gd',       vim.lsp.buf.definition,      description = 'Find definition' },
+--{ 'K',        vim.lsp.buf.hover,           description = 'Show help' },
 
-        -- NERDCommenter ------------------------------------------------------
-        {
-            '<leader>cl',
-            { n = '<Plug>NERDCommenterAlignLeft', x = '<Plug>NERDCommenterAlignLeft' },
-            opts = { silent = true },
-            description = 'Comment line(s) with delimiters aligned left.'
-        },
-        {
-            '<leader>cu',
-            { n = '<Plug>NERDCommenterUncomment', x = '<Plug>NERDCommenterUncomment' },
-            opts = { silent = true },
-            description = 'Uncomment line(s).'
-        },
-        {
-            '<leader>cy',
-            { n = '<Plug>NERDCommenterYank', x = '<Plug>NERDCommenterYank' },
-            opts = { silent = true },
-            description = 'Comment line(s) and yank the selection.'
-        },
-    },
-    autocmds = {
-        {
-            'InsertEnter',
-            function()
-                vim.opt.relativenumber = false
-            end,
-            description = 'Switch from relative to absolute line numbers'
-        },
-        {
-            'InsertLeave',
-            function()
-                vim.opt.relativenumber = true
-            end,
-            description = 'Switch from absolute to relative line numbers'
-        },
-        {
-            'BufWritePost',
-            function ()
-                if vim.bo.filetype == 'python' then
-                    vim.api.nvim_command('!black %')
-                end
-            end,
-            description = 'Run black formatter on autosave'
-        },
-    }
-})
+---- diagnostics
+--{ '<space>e', vim.diagnostic.open_float,   description = 'Open diagnostics' },
+--{ '<space>q', vim.diagnostic.set_loc_list, description = 'List diagnostics' },
+--{ '[d',       vim.diagnostic.goto_prev,    description = 'GoTo previous diagnoctic' },
+--{ ']d',       vim.diagnostic.goto_next,    description = 'GoTo next diagnostic' },
+
+--{
+--    '<space>f',
+--    function()
+--        vim.lsp.buf.format { async = true }
+--    end,
+--    description = 'Format current buffer'
+--},
